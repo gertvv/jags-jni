5b77b6d7132fcc43a9bd4ed537d877d537d1761e
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2010-02-01 16:17:20 +0100

    Put license on everything

--- src/java/src/main/java/fr/iarc/jags/model/Monitor.java
+++ src/java/src/main/java/fr/iarc/jags/model/Monitor.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of JAGS-JNI (JNI wrapper for the JAGS MCMC sampler).
+ * JAGS-JNI is distributed from http://github.com/gertvv/jags-jni/.
+ * Copyright (C) 2010  Gert van Valkenhoef.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package fr.iarc.jags.model;
 
 public class Monitor {
